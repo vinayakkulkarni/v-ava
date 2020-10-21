5eb07edd6e00627d6fe729ba617b9675a3abedaa
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-10-21 16:54:37 +0530

    fix: use `module-resolver` to perform `@/` imports

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  env: {
+    test: {
+      plugins: [
+        [
+          'module-resolver',
+          {
+            root: ['.'],
+            alias: {
+              '@': '.',
+              '~': '.',
+            },
+          },
+        ],
+      ],
+    },
+  },
 }
